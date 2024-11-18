ae04ed982a7e44b7e75dac611a7cf68f9ddaa33f
Radek <rad.szep@gmail.com>
2024-11-18 17:57:33 +0100

    Stoper początek

---
+++ Lab_03/stoper.js
@@ -0,0 +1,11 @@
+let timeInSeconds = 0;
+let timerInterval = null;
+
+function formatTime(seconds) {
+    if (seconds < 60) {
+        return `${seconds}s`;
+    }
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}min ${remainingSeconds}s`;
+}
