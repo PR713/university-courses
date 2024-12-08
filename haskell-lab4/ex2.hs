fbf41d7ff5055ea017f9a73b80abbdf1a14a7145
Radek <rad.szep@gmail.com>
2024-12-08 22:36:07 +0100

    08.12.2024

--- haskell-lab4/ex2.hs
+++ haskell-lab4/ex2.hs
@@ -27,3 +27,93 @@ data Cart2DVec'' a = MkCart2DVec'' {x::a, y::a}
 --yCoord'' :: Cart2DVec'' a -> a
 --yCoord'' (MkCart2DVec'' {y = yVal, x = _}) = yVal -- uwaga na kolejność x,y
 --
+
+
+
+
+
+
+
+
+-- sum type example (two constructors)
+data List a = EmptyL | Cons a (List a) deriving Show
+
+head' :: List a -> a
+head' EmptyL 	= error "head': the empty list has no head!"
+head' (Cons x xs) = x 
+
+
+
+
+
+-- enum type example (special case of sum type)
+data ThreeColors = Blue |
+                   White |
+                   Red
+
+type ActorName = String
+
+leadingActor :: ThreeColors -> ActorName
+leadingActor Blue  = "Juliette Binoche"
+leadingActor White = "Zbigniew Zamachowski"
+leadingActor Red   = "Irene Jacob"
+
+
+
+
+
+
+
+
+-- Definicja typu
+data Cart3DVec a = MkCart3DVec a a a
+
+-- Funkcje dostępowe
+xCoord3D :: Cart3DVec a -> a
+xCoord3D (MkCart3DVec x _ _) = x
+
+yCoord3D :: Cart3DVec a -> a
+yCoord3D (MkCart3DVec _ y _) = y
+
+zCoord3D :: Cart3DVec a -> a
+zCoord3D (MkCart3DVec _ _ z) = z
+
+
+--Definicja bez Mk
+data Cart3DVec' a = Cart3DVec' a a a
+-- Funkcje dostępowe
+xCoord3D' :: Cart3DVec' a -> a
+xCoord3D' (Cart3DVec' x _ _) = x
+
+yCoord3D' :: Cart3DVec' a -> a
+yCoord3D' (Cart3DVec' _ y _) = y
+
+zCoord3D' :: Cart3DVec' a -> a
+zCoord3D' (Cart3DVec' _ _ z) = z
+
+
+
+
+--record syntax
+data Cart3DVecRec a = Cart3DVecRec
+  { xCoord3D'' :: a
+  , yCoord3D'' :: a
+  , zCoord3D'' :: a
+  } deriving Show
+
+
+
+
+
+
+-- pole
+data Shape = Circle Float  -- Konstruktor koła z promieniem
+           | Rectangle Float Float -- Konstruktor prostokąta
+
+area :: Shape -> Float
+area (Circle r) = pi * r * r  -- Wzór na pole koła
+area (Rectangle a b) = a * b  -- Wzór na pole prostokąta
+
+
+
+
