62a15cbed2b08d1283a1c51c9f91d27e833a3261
Radek <rad.szep@gmail.com>
2024-12-21 15:11:25 +0100

    Ukończony kurs 5h, zajęło 8h

---
+++ KursReactVite/my-react-app7/src/App.jsx
@@ -0,0 +1,13 @@
+//import DigitalClock from "./DigitalClock.jsx";
+import ComponentA from "./ComponentA.jsx";
+
+function App() {
+
+    return (<ComponentA/>);
+
+    // return (
+    //     <><DigitalClock/>
+    //     </>);
+}
+
+export default App
---
+++ KursReactVite/my-react-app8/src/App.jsx
@@ -0,0 +1,11 @@
+//import MyComponent from "./MyComponent.jsx";
+import StopWatch from "./StopWatch.jsx";
+
+function App() {
+
+
+    return(<StopWatch/>);
+    //return(<MyComponent/>)
+}
+
+export default App
