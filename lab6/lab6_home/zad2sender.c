7df0558b278f16be157d93bd71f296d2c88d8293
PR713 <rad.szep@gmail.com>
2025-04-12 16:40:31 +0200

    finished, comms :)

--- lab6/lab6_home/zad2sender.c
+++ lab6/lab6_home/zad2sender.c
@@ -14,6 +14,18 @@ int main(int argc, char *argv[]){
     double step = atof(argv[3]);
 
     int fd_out = open("potok1", O_WRONLY);
+    //here it doesnt matter which program we will launch first, because
+    //both programs are doing open on "potok1", then on "potok2"
+    //so here we are being blocked on fd_out, but when we launch zad2 here
+    // we get "sb" who will read us so we are going to fd_in(...). By reversing
+    //launch that works the same way
+    //"Domyślnie otwieranie potoku nazwanego w trybie do odczytu blokuje do momentu,
+    // gdy jakiś inny proces otworzy potok w trybie do zapisu. Analogicznie w drugą stronę
+    // - otwieranie w trybie do zapisu blokuje do momentu, gdy ktoś inny otworzy w trybie do odczytu."
+
+    //There are no writers if all descriptors fd (to write) are closed,
+    //in case of stream unnamed fd[1]; until it doesn't happen then read, write, open
+    //cause blocking.
     int fd_in = open("potok2", O_RDONLY);
 
     write(fd_out, &start, sizeof(double));
--- lab6/lab6_in_class/kartkowka.c
+++ lab6/lab6_in_class/kartkowka.c
@@ -10,15 +10,20 @@ int main() {
 
     if (fork() == 0) {
         close(fd[1]);
-        //sleep(1); <- naprawia oczekiwanie
+        sleep(1);
         char c;
         while(read(fd[0], &c, 1) > 0)
-            printf("%c", c);
+            printf("%c\n", c);
         printf("END\n"); //\n potem możemy pisać w konsoli
+        //jeśli nic nie ma to zasypia (blokuje się), jeśli coś jest to czyta i dopóki
+        //są pisarze to czeka na więcej
     } else {
         close(fd[0]);
         write(fd[1], "ABC", 3);
         //close(fd[1]);
+        sleep(5);
+        //printuje się END mimo że close(fd[1]) zakomentowane bo rodzic po prostu robi return 0 i deskryptory
+        //same się zamykają, jak damy sleep(5) to mamy A B C i dopiero po 5 sekundach END
     }
 
     return 0;
