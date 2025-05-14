1a83315ffee24ca1756abac49a1ae8798f40fff2
Radek <rad.szep@gmail.com>
2025-05-14 11:14:20 +0200

    Part 1

---
+++ Report3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Podaj nazwę produktu: ");
+String? prodName = Console.ReadLine();
+
+ProdContext productContext = new ProdContext();
+Product product = new Product { ProductName = prodName };
+productContext.Products.Add(product);
+productContext.SaveChanges();
+
+
+var query = from prod in productContext.Products
+            select prod.ProductName;
+
+
+foreach (var pName in query)
+{
+Console.WriteLine(pName);
+}   
