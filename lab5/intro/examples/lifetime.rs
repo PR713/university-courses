a11792ce98af32821c891b139c0baba11e9b976c
Radek <rad.szep@gmail.com>
2025-05-20 20:45:36 +0200

    finished

--- lab5/intro/examples/lifetime.rs
+++ lab5/intro/examples/lifetime.rs
@@ -15,6 +15,36 @@ fn longer_array<'a>(a : &'a[i32], b : &'a[i32]) -> &'a[i32] {
     }
 }
 
+
+
+#[derive(Debug)]
+struct Introduction<'a> {
+    intro : &'a str
+}
+
+impl<'a> Introduction<'a> {
+    fn print(&self) {
+        println!("{}", self.intro);
+    }
+
+    fn get_intro(&self) -> &str { //lifetime elision rules
+        self.intro
+    }
+}
+
+
+//
+
+fn get_sample_text() -> &'static str {
+    "Just a sample text" //&str, rust nie ma samodzielnego str - dynamically sized type
+}
+
+// fn get_sample_text2() -> &'static str {
+//     String::from("Just a sample text").as_str() //źle bo zwracamy &str czyli tylko
+//podgląd do tego co ma w sobie String
+// }
+
+
 fn main() {
     let ref_x;
     {
@@ -34,4 +64,20 @@ fn main() {
 
 
     // structures instances attributes lifetime
+
+    let text = String::from("Introduction to a long text. The rest of long text with many sentences.");
+
+    let intro = text.split('.').next().expect("Introduction needs a '.'");
+
+    let i = Introduction { intro };
+
+    println!("{:?}", i);
+    i.print();
+    let x = i.get_intro();
+    println!("{:?}", x);
+
+
+
+    // static lifetime specifiers
+
 }
