e4effca94a0b77a28cef89afbd588a07e1a0cafb
Radek <rad.szep@gmail.com>
2025-10-26 23:58:57 +0100

    move project to another repository

--- Projekt_stats/WDAI_Project/backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {authenticate} from "../middleware/authMiddleware.js";
-import {addAnOrder, getOrders} from "../controllers/orderController.js";
-const router = express.Router();
-
-router.post('/', authenticate, addAnOrder);
-router.get('/userId', authenticate, getOrders);
-
-export default router;
